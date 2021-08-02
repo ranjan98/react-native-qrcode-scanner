830a591e9974ebc5e1faf18fd1360ec10e50ae51
Ranjan Goyal <ranjangoyal1@gmail.com>
2021-08-02 15:49:33 +0530

    Clean Code 2

--- navigation/QRCodeStackScreen.js
+++ navigation/QRCodeStackScreen.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import QRCodeScannerScreen from '../screens/QRScanner/QRCodeScannerScreen';
 import QRScannedScreen from '../screens/QRScanner/QRScannedScreen';
 
